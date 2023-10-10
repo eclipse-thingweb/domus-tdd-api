ecbe0349a8e9d9e4e861578d91adaca5da8d220f
Elodie Thiéblin <ethieblin@logilab.fr>
2023-10-10 11:59:55 +0200

    feat: update jsonld.js

--- tdd/lib/frame-jsonld.js
+++ tdd/lib/frame-jsonld.js
@@ -1,19 +1,14 @@
-const jsonld = require("../jsonld/lib/jsonld.js");
+const jsonld = require("jsonld");
 const data = process.argv[2];
 const framedata = process.argv[3];
 
 async function frame() {
-  try {
-    const doc = await jsonld.fromRDF(data, { format: "application/n-quads", useNativeTypes: "true" });
-    try {
-      const framed = await jsonld.frame(doc, JSON.parse(framedata));
-      return JSON.stringify(framed, null, 2);
-    } catch (error) {
-      throw new Error(418);
-    }
-  } catch (error) {
-    throw new Error(418);
-  }
+  const doc = await jsonld.fromRDF(data, {
+    format: "application/n-quads",
+    useNativeTypes: "true",
+  });
+  const framed = await jsonld.frame(doc, JSON.parse(framedata));
+  return JSON.stringify(framed, null, 2);
 }
 
 frame().then(console.log);
